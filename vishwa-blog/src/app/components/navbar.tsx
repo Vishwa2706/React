648280283d9b00bac8c247811196477e9ff739ab
Vishwa Settu <vishwa@propelinc.com>
2025-05-06 18:29:10 +0530

    navbar design updated

--- vishwa-blog/src/app/components/navbar.tsx
+++ vishwa-blog/src/app/components/navbar.tsx
@@ -1,13 +1,72 @@
-import Link from 'next/link';
+"use client";
+
+import "../../styles/navbar.scss";
+
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme((prev) => !prev);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/posts">Posts</Link></li>
-      </ul>
+    <nav className="navbar">
+      <div className="logo-div">
+        <img
+          src="/logo-light.svg"
+          alt="Logo"
+          className="logo-image"
+          loading="lazy"
+        />
+      </div>
+
+      <div className="navigation-tabs">
+        <ul>
+          <li>
+            <Link href="/" className={pathname === "/" ? "active" : ""}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              className={pathname === "/about" ? "active" : ""}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/posts"
+              className={pathname === "/posts" ? "active" : ""}
+            >
+              Posts
+            </Link>
+          </li>
+          <li>
+            <button
+              className={`theme-btn theme-btn-desktop ${
+                isDarkTheme ? "dark" : "light"
+              }`}
+              onClick={toggleTheme}
+            >
+              <span className="toggle-thumb" />
+              {isDarkTheme ? (
+                <HiOutlineMoon className="icon" />
+              ) : (
+                <HiOutlineSun className="icon" />
+              )}
+            </button>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
